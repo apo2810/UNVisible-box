a5c828dcd284573b9302af56733fe0ea096c4dd2
apo2810 <apolline.fert@gmail.com>
2023-06-27 00:12:37 +0200

    ajout led + boutons
    ajout des led et de deux boutons

--- Box-UNvisible/Box-UNvisible.ino
+++ Box-UNvisible/Box-UNvisible.ino
@@ -1,13 +1,18 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Adafruit_NeoPixel.h>
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
-
+#define NUM_LEDS        12        // Nombre de LED dans le cercle
+#define DATA_PIN        2         // Broche de données de la LED
+#define BOUTON_PIN      1
+#define BOUTON_PIN2     3
 #define SS_PIN 10
 #define RST_PIN 9
 #define DFPLAYER_RX_PIN 7
 #define DFPLAYER_TX_PIN 8
 
+Adafruit_NeoPixel leds(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Initialisation du module RFID
 SoftwareSerial mySoftwareSerial(DFPLAYER_RX_PIN, DFPLAYER_TX_PIN);  // Initialisation du port série pour le lecteur MP3
 DFRobotDFPlayerMini myDFPlayer;  // Création d'une instance du lecteur MP3
@@ -16,7 +21,12 @@ void setup() {
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
-
+  leds.begin();               // Initialise la bibliothèque NeoPixel
+  leds.show();                // Éteint toutes les LED au démarrage
+  digitalWrite(BOUTON_PIN,HIGH);
+  pinMode(BOUTON_PIN, INPUT); 
+  digitalWrite(BOUTON_PIN2,HIGH);
+  pinMode(BOUTON_PIN2, INPUT);  
   mySoftwareSerial.begin(9600);
   if (!myDFPlayer.begin(mySoftwareSerial)) {
     Serial.println("Erreur lors de la communication avec le lecteur MP3 !");
@@ -36,12 +46,9 @@ void loop() {
       cardUID.toUpperCase();
     }
     playSoundFromCardUID(cardUID);  // Joue la musique correspondante
-    delay(1000);  // Attente pour éviter de lire plusieurs fois la même carte
+    delay(10000);  // Attente pour éviter de lire plusieurs fois la même carte
   }
 }
-
-
-
 
 void playSound(uint16_t soundNumber) {
   myDFPlayer.play(soundNumber);
@@ -53,13 +60,29 @@ void playSound(uint16_t soundNumber) {
 void playSoundFromCardUID(String cardUID) {
   if (cardUID == "49315499") {
     playSound(1);  // Joue la piste 1 du lecteur MP3
+    if (digitalRead(BOUTON_PIN) == LOW) {  // Vérifie si le bouton est appuyé
+    allumerCercleLED(0, 255, 0);         // Allume le cercle de LED en vert
+    delay(3000);                         // Attend 3 secondes
+    leds.clear();                        // Éteint toutes les LED
+    leds.show();                         // Met à jour l'affichage des LED
+    }
+    else if (digitalRead(BOUTON_PIN2) == LOW) {  // Vérifie si le bouton est appuyé
+    allumerCercleLED(255, 0, 0);         // Allume le cercle de LED en vert
+    delay(3000);                         // Attend 3 secondes
+    leds.clear();                        // Éteint toutes les LED
+    leds.show();                         // Met à jour l'affichage des LED
+    }
+    else {  
+    allumerCercleLED(0, 0, 0);         // Allume le cercle de LED en vert                 
+    leds.clear();                        // Éteint toutes les LED
+  }
+  } else if (cardUID == "5B1D37BB") {
+    playSound(1);  // Joue la piste 2 du lecteur MP3
   } else if (cardUID == "CCDB7689") {
-    playSound(2);  // Joue la piste 2 du lecteur MP3
-  } else if (cardUID == "15550C09") {
     playSound(3);  // Joue la piste 3 du lecteur MP3
   } else if (cardUID == "644CF11D") {
     playSound(4);  // Joue la piste 4 du lecteur MP3
-  } else if (cardUID == "5B1D37BB") {
+  } else if (cardUID == "15550C09") {
     playSound(5);  // Joue la piste 5 du lecteur MP3
   } else {
     Serial.println("Carte UID non reconnue !");
@@ -68,4 +91,10 @@ void playSoundFromCardUID(String cardUID) {
 
 void playStartupSound() {
   playSound(0);  // Joue la piste 0 du lecteur MP3 (musique de démarrage)
+}
+void allumerCercleLED(byte r, byte g, byte b) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds.setPixelColor(i, r, g, b);  // Allume chaque LED avec la couleur spécifiée
+  }
+  leds.show();                       // Met à jour l'affichage des LED
 }
