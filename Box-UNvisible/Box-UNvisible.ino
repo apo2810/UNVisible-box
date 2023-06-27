e84f27c962d4b4fdbeace07c3836a4b699dd9de6
apo2810 <apolline.fert@gmail.com>
2023-06-28 01:10:13 +0200

    Boutons + uid
    pour chaque uid de carte est associé un bon bouton la leds verte s'allume si c'est le bon bouton qui est appuyé la led rouge s'allume si c'est un autre

--- Box-UNvisible/Box-UNvisible.ino
+++ Box-UNvisible/Box-UNvisible.ino
@@ -1,40 +1,66 @@
 #include <SPI.h>
 #include <MFRC522.h>
-#include <Adafruit_NeoPixel.h>
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
-#define NUM_LEDS        12        // Nombre de LED dans le cercle
-#define DATA_PIN        2         // Broche de données de la LED
-#define BOUTON_PIN      1
-#define BOUTON_PIN2     3
+
 #define SS_PIN 10
 #define RST_PIN 9
 #define DFPLAYER_RX_PIN 7
 #define DFPLAYER_TX_PIN 8
+#define BUTTON_1_PIN 2
+#define BUTTON_2_PIN 3
+#define BUTTON_3_PIN 4
+#define BUTTON_4_PIN 5
+#define BUTTON_5_PIN 6
+#define LED_GREEN_PIN A0
+#define LED_RED_PIN A1
 
-Adafruit_NeoPixel leds(NUM_LEDS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Initialisation du module RFID
 SoftwareSerial mySoftwareSerial(DFPLAYER_RX_PIN, DFPLAYER_TX_PIN);  // Initialisation du port série pour le lecteur MP3
 DFRobotDFPlayerMini myDFPlayer;  // Création d'une instance du lecteur MP3
+
+boolean button1Pressed = false;
+boolean button2Pressed = false;
+boolean button3Pressed = false;
+boolean button4Pressed = false;
+boolean button5Pressed = false;
+boolean correctButtonPressed = false;
+
+// Définition des UID des badges
+String uidButton1 = "644CF11D";
+String uidButton2 = "CCDB7689";
+String uidButton3 = "15550C09";
+String uidButton4 = "5B1D37BB";
+String uidButton5 = "49315499";
+
+int currentTrackNumber = 0;
 
 void setup() {
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
-  leds.begin();               // Initialise la bibliothèque NeoPixel
-  leds.show();                // Éteint toutes les LED au démarrage
-  digitalWrite(BOUTON_PIN,HIGH);
-  pinMode(BOUTON_PIN, INPUT); 
-  digitalWrite(BOUTON_PIN2,HIGH);
-  pinMode(BOUTON_PIN2, INPUT);  
+
   mySoftwareSerial.begin(9600);
   if (!myDFPlayer.begin(mySoftwareSerial)) {
     Serial.println("Erreur lors de la communication avec le lecteur MP3 !");
     while (true);
   }
 
+  pinMode(BUTTON_1_PIN, INPUT);
+  pinMode(BUTTON_2_PIN, INPUT);
+  pinMode(BUTTON_3_PIN, INPUT);
+  pinMode(BUTTON_4_PIN, INPUT);
+  pinMode(BUTTON_5_PIN, INPUT);
+  pinMode(LED_GREEN_PIN, OUTPUT);
+  pinMode(LED_RED_PIN, OUTPUT);
+
+  digitalWrite(BUTTON_1_PIN, HIGH);
+  digitalWrite(BUTTON_2_PIN, HIGH);
+  digitalWrite(BUTTON_3_PIN, HIGH);
+  digitalWrite(BUTTON_4_PIN, HIGH);
+  digitalWrite(BUTTON_5_PIN, HIGH);
+
   Serial.println("Prêt !");
-  playStartupSound();  // Joue une musique au démarrage
 }
 
 void loop() {
@@ -43,58 +69,67 @@ void loop() {
     for (byte i = 0; i < mfrc522.uid.size; i++) {
       cardUID += String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
       cardUID += String(mfrc522.uid.uidByte[i], HEX);
-      cardUID.toUpperCase();
     }
-    playSoundFromCardUID(cardUID);  // Joue la musique correspondante
-    delay(10000);  // Attente pour éviter de lire plusieurs fois la même carte
+    cardUID.toUpperCase();
+    playSoundFromCardUID(cardUID);  // Joue le son correspondant
+    delay(1000);  // Attente pour éviter de lire plusieurs fois la même carte
+
+    // Réinitialise l'état des boutons
+    button1Pressed = false;
+    button2Pressed = false;
+    button3Pressed = false;
+    button4Pressed = false;
+    button5Pressed = false;
+    correctButtonPressed = false;
+    
   }
+
+  // Vérifie l'état des boutons
+  checkButton(BUTTON_1_PIN, button1Pressed, 1);
+  checkButton(BUTTON_2_PIN, button2Pressed, 2);
+  checkButton(BUTTON_3_PIN, button3Pressed, 3);
+  checkButton(BUTTON_4_PIN, button4Pressed, 4);
+  checkButton(BUTTON_5_PIN, button5Pressed, 5);
 }
 
-void playSound(uint16_t soundNumber) {
-  myDFPlayer.play(soundNumber);
-  delay(500);  // Attente pour laisser le temps au lecteur MP3 de démarrer la lecture
-  while (myDFPlayer.available()) {
-    myDFPlayer.readType();
-  }
-}
 void playSoundFromCardUID(String cardUID) {
-  if (cardUID == "49315499") {
-    playSound(1);  // Joue la piste 1 du lecteur MP3
-    if (digitalRead(BOUTON_PIN) == LOW) {  // Vérifie si le bouton est appuyé
-    allumerCercleLED(0, 255, 0);         // Allume le cercle de LED en vert
-    delay(3000);                         // Attend 3 secondes
-    leds.clear();                        // Éteint toutes les LED
-    leds.show();                         // Met à jour l'affichage des LED
-    }
-    else if (digitalRead(BOUTON_PIN2) == LOW) {  // Vérifie si le bouton est appuyé
-    allumerCercleLED(255, 0, 0);         // Allume le cercle de LED en vert
-    delay(3000);                         // Attend 3 secondes
-    leds.clear();                        // Éteint toutes les LED
-    leds.show();                         // Met à jour l'affichage des LED
-    }
-    else {  
-    allumerCercleLED(0, 0, 0);         // Allume le cercle de LED en vert                 
-    leds.clear();                        // Éteint toutes les LED
-  }
-  } else if (cardUID == "5B1D37BB") {
-    playSound(1);  // Joue la piste 2 du lecteur MP3
-  } else if (cardUID == "CCDB7689") {
-    playSound(3);  // Joue la piste 3 du lecteur MP3
-  } else if (cardUID == "644CF11D") {
-    playSound(4);  // Joue la piste 4 du lecteur MP3
-  } else if (cardUID == "15550C09") {
-    playSound(5);  // Joue la piste 5 du lecteur MP3
+  if (cardUID == uidButton1) {
+    playSound(1);
+  } else if (cardUID == uidButton2) {
+    playSound(1);
+  } else if (cardUID == uidButton3) {
+    playSound(3);
+  } else if (cardUID == uidButton4) {
+    playSound(4);
+  } else if (cardUID == uidButton5) {
+    playSound(5);
   } else {
     Serial.println("Carte UID non reconnue !");
   }
 }
 
-void playStartupSound() {
-  playSound(0);  // Joue la piste 0 du lecteur MP3 (musique de démarrage)
-}
-void allumerCercleLED(byte r, byte g, byte b) {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds.setPixelColor(i, r, g, b);  // Allume chaque LED avec la couleur spécifiée
+void playSound(int trackNumber) {
+  if (trackNumber != currentTrackNumber) {
+    currentTrackNumber = trackNumber;
+    myDFPlayer.stop();
+    myDFPlayer.play(trackNumber);
+    delay(100);
   }
-  leds.show();                       // Met à jour l'affichage des LED
+}
+
+void checkButton(int buttonPin, boolean &buttonPressed, int buttonNumber) {
+  if (digitalRead(buttonPin) == LOW && !buttonPressed) {
+    buttonPressed = true;
+    if (buttonNumber == currentTrackNumber) {
+      Serial.println("Bouton " + String(buttonNumber) + " : Bon bouton appuyé !");
+      digitalWrite(LED_GREEN_PIN, HIGH);
+      digitalWrite(LED_RED_PIN, LOW);
+    } else {
+      Serial.println("Bouton " + String(buttonNumber) + " : Mauvais bouton appuyé !");
+      digitalWrite(LED_GREEN_PIN, LOW);
+      digitalWrite(LED_RED_PIN, HIGH);
+    }
+  } else if (digitalRead(buttonPin) == HIGH && buttonPressed) {
+    buttonPressed = false;
+  }
 }
